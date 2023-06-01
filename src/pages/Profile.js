30011227d89e68c76df93182690676264e54f6c0
aniyuksh <ozaaniyuksh@gmail.com>
2023-06-01 16:33:48 +0530

    added cart and wishlist functionality

---
+++ src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/navbar";
+
+const Profile = () => {
+  return (
+    <div>
+      <Navbar />
+      <section></section>
+    </div>
+  );
+};
+
+export default Profile;
